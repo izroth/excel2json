305fffcd03406711f95013d0102823d08f9c89e8
izroth <lakhansharma889027@gmail.com>
2023-09-12 13:42:01 +0530

    done

---
+++ node_modules/excel-2-json/node_modules/excel-2-json/node_modules/excel-2-json/node_modules/excel-2-json/node_modules/excel-2-json/excel2json.js
@@ -0,0 +1,13 @@
+//create a module to convert excel data to json
+
+var xlsx = require('xlsx');
+var fs = require('fs');
+
+const excel2json = (excelFile, sheetName) => {
+    var workbook = xlsx.readFile(excelFile);
+    var sheet_name_list = workbook.SheetNames;
+    var json = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    return json;
+}
+
+module.exports = excel2json;
